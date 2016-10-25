745585654c75db6144e4e3c1cb7aa07f7e3275ea
iwwwolf <truewebcoder@gmail.com>
2016-10-25 18:15:07 +0600

    vertical slider

--- assets/great-slider/script.js
+++ assets/great-slider/script.js
@@ -1,30 +1,4 @@
 if($('#greatSlider').length){
-    /*if(winWidth >= 992) {
-    	var headerHeight = $('#header .header__container').outerHeight();
-
-    	$('body').append('<style>.great-slider__item:after{top: ' + headerHeight + 'px;}</style>');
-
-    }*/
-    /*var greatSlider = new Swiper ('#greatSlider', {
-        direction: 'vertical',
-        //spaceBetween: 0,
-        //effect: 'fade',
-        //preloadImages: false,
-        //lazyLoading: true,
-        //speed: 400,
-        autoheight: true,
-        slidesPerView: 2,
-        //autoplay: 4000,
-        //loop: true,
-        //simulateTouch: false,
-        pagination: '#greatSliderPages',
-        nextButton: '#greatSliderNext',
-        prevButton: '#greatSliderPrev',
-        //paginationHide: false,
-        paginationClickable: true
-        //runCallbacksOnInit: true
-    });*/
-
     if(mobile){
         $('#greatSlider .great-slider__wrapper').slick({
             slidesToShow: 1,
