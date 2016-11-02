f0817bf7e89fd68e99face5933b148327c635eb4
iwwwolf <truewebcoder@gmail.com>
2016-11-02 17:59:20 +0600

    quotes-info ajax

--- assets/mini-menu/script.js
+++ assets/mini-menu/script.js
@@ -27,6 +27,82 @@ if($('.mini-menu__menu').length){
 		});
 	}*/
 
+	$('.quotes-info__parent').on('click', '.mini-menu .link', function(e){
+		e.preventDefault();
+		var self = $(this).parent();
+		var title = self.data('title');
+		var parent = self.parents('.quotes-info').attr('id');
+		$.ajax({
+			url: 'http://kase.dev/build/server/quotes-info/' + title + '.html',
+			dataType: 'html',
+			success: function(data){
+				$('#' + parent).html(data);
+				self.parent().find('.active').removeClass('active');
+				self.addClass('active');
+				
+
+				$('.mini-menu__menu').each(function(index, el){
+					var menuLeftPos = $(el).offset().left;
+					var menuWidth = $(el).width();
+					$(el).css({
+						width: 0, 
+						display: 'none', 
+						opacity: 1
+					});
+					if(menuLeftPos > winWidth - menuWidth){
+						$(el).css({
+							left: 'auto', 
+							right: '100%',
+							marginLeft: 0,
+							marginRight: '5px'
+						});
+					}
+				});
+
+				$('.mini-menu__link').on('click', function(e){
+					var target = $(this).parent().find('.mini-menu__menu');
+					var targetHeight = target.height();
+
+					e.preventDefault();
+					if(!$(this).hasClass('active')){
+						$(this).addClass('active');
+						target.css({
+							height: '4px',
+							padding: 0, 
+							display: 'block'
+						}).animate({
+							width: '290px',
+							padding: '0 25px'
+						}, 150, '', function(){
+							target.animate({
+								padding: miniMenuPadding,
+								height: targetHeight
+							});
+						}, 180);
+					} else {
+						$(this).removeClass('active');
+
+						target.animate({
+							height: '4px',
+							padding: '0 25px'
+						}, 150, '', function(){
+							target.animate({
+								padding: 0,
+								width: 0
+							}, 180, '', function(){
+								target.css({
+									display: 'none',
+									height: 'auto',
+									padding: miniMenuPadding
+								});
+							});
+						});
+					}
+				});
+			}
+		});
+	});
+
 	$('.mini-menu__menu').each(function(index, el){
 		var parentId = $(el).parents('.quotes-info').attr('id');
 		var menuLeftPos = $(el).offset().left;
@@ -92,20 +168,4 @@ if($('.mini-menu__menu').length){
 			});
 		}
 	});
-	$('.mini-menu__menu ul a').on('click', function(e){
-		e.preventDefault();
-		var self = $(this).parent();
-		var title = self.data('title');
-		var parent = self.parents('.quotes-info').attr('id');
-		$.ajax({
-			url: 'http://kase.dev/build/server/quotes-info/' + title + '.html',
-			dataType: 'html',
-			success: function(data){
-				$('#' + parent).html(data);
-				self.parent().find('.active').removeClass('active');
-				self.addClass('active');
-				console.log(self);
-			}
-		});
-	})
 }
