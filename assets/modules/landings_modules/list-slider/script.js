2662a7aaf5f46b13d160a3d8ea83792b30dd1253
iwwwolf <truewebcoder@gmail.com>
2016-12-06 19:03:58 +0600

    just commit

--- assets/modules/landings_modules/list-slider/script.js
+++ assets/modules/landings_modules/list-slider/script.js
@@ -1,15 +1,76 @@
-if($('.list-slider').length){
-	$(".list-slider").each(function(index, element){
-		var $this = $(this);
-		var homeNews = new Swiper ($this, {
-	    	loop: false,
-	    	speed: 400,
-	    	autoheight: true,
-	    	slidesPerView: 4,
-	    	autoplay: false,
-	    	pagination: $this.parent().find('.home-news__pagination'),
-	    	paginationHide: false,
-	    	paginationClickable: true
+if($('#listSlider').length){
+	var listSlider = $('#listSlider');
+	var listSliderCount = $('#listSlider .list-slider__item').length;
+
+	if(winWidth >= 992){
+		if(listSliderCount < 4) {
+			listSlider.slick({
+				speed: 900,
+				slidesToShow: listSliderCount,
+				slidesToScroll: 1,
+				dots: true,
+				//dotsClass: 'great-slider__pagination',
+				infinite: false,
+				arrows: false,
+				autoplay: false
+			});
+		} else {
+			listSlider.slick({
+				speed: 900,
+				slidesToShow: 4,
+				slidesToScroll: 4,
+				dots: true,
+				//dotsClass: 'great-slider__pagination',
+				infinite: false,
+				arrows: false,
+				autoplay: false
+			});
+		}
+	} else {
+		listSlider.slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			speed: 900,
+			dots: true,
+			//dotsClass: 'great-slider__pagination',
+			autoplay: false,
+			infinite: false,
+			arrows: false,
+			responsive: [
+			    {
+			      breakpoint: 991,
+			      settings: {
+			        slidesToShow: 3,
+			        slidesToScroll: 3,
+			        speed: 900,
+        			dots: true,
+        			//dotsClass: 'great-slider__pagination',
+        			autoplay: false
+			      }
+			    },
+			    {
+			      breakpoint: 768,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 2,
+			        speed: 900,
+        			dots: true,
+        			//dotsClass: 'great-slider__pagination',
+        			autoplay: false
+			      }
+			    },
+			    {
+			      breakpoint: 540,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1,
+			        speed: 900,
+        			dots: true,
+        			//dotsClass: 'great-slider__pagination',
+        			autoplay: false
+			      }
+			    }
+		    ]
 		});
-	});
+	}
 }
