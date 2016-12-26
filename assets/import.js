8d8f45004585c51fa308c23dd88b413cec77c753
SPavelhjk <SPavel.hjk@mail.ru>
2016-12-26 11:13:48 +0600

    Filled my part

--- assets/import.js
+++ assets/import.js
@@ -1,23 +1,10 @@
 $(document).ready(function(){
 
 	//= require "default.js"
-	//= require "modules/header-search/script.js"
-	//= require "modules/great-slider/script.js"
-	//= require "modules/menu/script.js"
-	//= require "modules/mini-menu/script.js"
-	//= require "modules/home-news/script.js"
-	//= require "modules/regulations/script.js"
-	//= require "modules/accordion/script.js"
-	//= require "modules/sorting-table/script.js"
-	//= require "modules/currency-tabs/script.js"
-	//= require "modules/search-block/script.js"
-	//= require "modules/tools-form/script.js"
 	
-	//= require "modules/landings_modules/head-rect/script.js"
-	//= require "modules/landings_modules/ipo-block/script.js"
-	//= require "modules/landings_modules/list-slider/script.js"
-	//= require "modules/landings_modules/infrastructure-scheme/script.js"
-	//= require "modules/landings_modules/slider-white-block/script.js"
+	//= require "modules/**/script.js"
+	//= require "modules/landings_modules/**/script.js"
+	
 
 	/* require "modules/trade-info/script.js"
 	 require "modules/donut-chart/script.js"*/
